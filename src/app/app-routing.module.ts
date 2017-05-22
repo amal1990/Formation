21f0d9259ed2412707cfc1011b9331bb2a62c7df
abahloul <amal.bahloul@groupe-telnet.net>
2017-05-22 16:19:28 +0100

    Initial import

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { AboutComponent } from './about/about.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {  path: 'about', component: AboutComponent},
+  {  path: 'formation', loadChildren: './formation/formation.module#FormationModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
